8f61859538bf85d515f90eba36e65b725cd1c87b
suman rajput <44019903+sumandeepak8@users.noreply.github.com>
2019-01-07 23:59:18 +0530

    Modified all files and added new image

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
-let player1;
-let player2;
+let player1 = 'firstPlayer';
+let player2 = 'secondPlayer';
 let player1Moves = [];
 let player2Moves = [];
 const wininigConditions = [['1', '2', '3'], ['4', '5', '6'], ['7', '8', '9'], ['1', '5', '9'], ['1', '4', '7'], ['2', '5', '8'], ['3', '5', '7'], ['3', '6', '9']]
+let turnCounter = 0;
 
 const isSubset = function (wininigConditions, moves) {
   return wininigConditions.some((x) => {
@@ -13,50 +14,89 @@ const isSubset = function (wininigConditions, moves) {
 };
 
 const declareWinner = function (symbol) {
-  let moves = [];
-  let player;
-  if (symbol == 'x') {
-    moves = player1Moves;
-    player = player1
+  let symbolDetails = {
+    'X': {
+      moves: player1Moves,
+      player: player1,
+    },
+    '0': {
+      moves: player2Moves,
+      player: player2,
+    },
   };
-  if (symbol == 'o') {
-    moves = player2Moves;
-    player = player2;
-  };
+  let player = symbolDetails[symbol]['player'];
+  let moves = symbolDetails[symbol]['moves'];
 
+  if (!isSubset(wininigConditions, moves) && player1Moves.length == 5) {
+    document.getElementById('turn').innerHTML = " Match is drawn ";
+    document.getElementById('turn').setAttribute("style", "background-color:green");
+  }
   if (isSubset(wininigConditions, moves)) {
-    document.getElementById('winner').innerHTML = player + '  has won the match';
-    window.stop();
+    document.getElementById('turn').innerHTML = player + '  has won the match';
+    document.getElementById('turn').setAttribute("style", "background-color:green");
+    for (let id = 1; id <= 9; id++) {
+      document.getElementById(id.toString()).setAttribute('disabled', 'disabled');
+    }
   }
 }
 
 const moveToggler = function (id) {
-  let value = +document.getElementById('counter').value;
-  document.getElementById('counter').value = value + 1;
-  if (value % 2) {
+  let value = turnCounter;
+  let isOdd = value % 2;
+  if (isOdd) {
     player2Moves.push(id);
-    return 'o';
+    return '0';
   }
   player1Moves.push(id);
-  return 'x';
+  return 'X';
 }
 
+const firstTurn = function () {
+  document.getElementById('turn').innerHTML = player1 + "'s turn ";
+};
+
+const whoseTurn = function (symbol) {
+  if (symbol == 'X')
+    document.getElementById('turn').innerHTML = player2 + "'s turn ";
+  if (symbol == '0')
+    document.getElementById('turn').innerHTML = player1 + "'s turn ";
+};
+
 const getPlayer1 = function () {
-  player1 = document.getElementById('player1').value;
-  document.getElementById('x').innerHTML = "your symbol is 'x' ";
+  let name = document.getElementById('player1').value;
+  if (name != '')
+    player1 = name;
+  document.getElementById('X').innerHTML = player1 + "'s playing symbol is 'X' ";
   document.getElementById('player1').setAttribute('disabled', 'disabled');
 }
 
 const getPlayer2 = function () {
-  player2 = document.getElementById('player2').value;
-  document.getElementById('o').innerHTML = "your symbol is 'o' ";
+  let name = document.getElementById('player2').value;
+  if (name != '')
+    player2 = name;
+  document.getElementById('0').innerHTML = player2 + "'s playing symbol is '0' ";
   document.getElementById('player2').setAttribute('disabled', 'disabled');
-
 }
 
 const updateBoard = function (id) {
   let symbol = moveToggler(id);
+  whoseTurn(symbol);
   document.getElementById(id).value = symbol;
   document.getElementById(id).setAttribute('disabled', 'disabled');
+  turnCounter += 1;
   declareWinner(symbol);
+}
+
+const setFocus = function () {
+  if (event.keyCode == 13) {
+    document.getElementById('player2').focus();
+  }
+};
+
+const showBoard = function () {
+  getPlayer1();
+  getPlayer2();
+  firstTurn();
+  document.getElementById('board').style.visibility = 'visible';
+  document.getElementById('turn').style.visibility = 'visible';
 }
